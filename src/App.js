d66308ae62ee2143d1c91c797e0a240c82501d35
Trent <mr.peschke@gmail.com>
2024-01-13 19:45:57 -0700

    bug fixing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
 import Quill from './quill';
 import axios from 'axios';
 
 function App() {
-  let enchantedItem = {
-    name: null,
-    description: null,
-    itemcategory: null,
-    power: null,
-    price: null,
-    history: null,
-    major: false,
-    size: null
+  const [name, setName] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [itemcategory, setItemcategory] = useState(null);
+  const [power, setPower] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [history, setHistory] = useState(null);
+  const [major, setMajor] = useState(null);
+  const [size, setSize] = useState(null);
+
+  const saveItem = () => {
+    axios.post('http://localhost:3535' + '/api/addEnchantedItem', {name, description, itemcategory, power, price, history, major, size}).then(({ data }) => {
+      window.location.reload(false);
+    })
   }
 
   const updateProperty = (type, value) => {
-    enchantedItem[type] = value
-  }
-
-  const saveItem = () => {
-    axios.post('http://localhost:3535' + '/api/addEnchantedItem', enchantedItem).then(({ data }) => {
-      window.location.reload(false);
-    })
+    if (type === 'description') {
+      setDescription(value)
+    } else if (type === 'power') {
+      setPower(value)
+    } else if (type === 'price') {
+      setPrice(value)
+    } else if (type === 'history') {
+      setHistory(value)
+    }
   }
 
   return (
     <div className="App">
       <h1>Add Enchanted Item</h1>
-      <input placeholder="Name" onChange={e => updateProperty('name', e.target.value)}></input>
+      <input placeholder="Name" onChange={e => setName(e.target.value)}></input>
       <Quill type="description" updateProperty={updateProperty} />
       <h2>Item Category</h2>
-      <select onChange={e => updateProperty('itemcategory', e.target.value)}>
+      <select onChange={e => setItemcategory(e.target.value)}>
         <option>Academic Tools</option>
         <option>Alchemical Substances</option>
         <option>Adventuring Gear</option>
@@ -73,9 +79,10 @@ function App() {
       <Quill type="power" updateProperty={updateProperty} />
       <Quill type="price" updateProperty={updateProperty} />
       <Quill type="history" updateProperty={updateProperty} />
-      <button className={enchantedItem.major ? 'button major' : 'button'} onClick={_ => updateProperty('major', !enchantedItem.major)}>Major?</button>
+      <button className={major ? 'button major' : 'button'} onClick={_ => setMajor(!major)}>Major?</button>
+      <p>{major ? "Yes" : "No"}</p>
       <h2>Size</h2>
-      <select onChange={e => updateProperty('size', e.target.value)}>
+      <select onChange={e => setSize(e.target.value)}>
         <option>F</option>
         <option>D</option>
         <option>T</option>
